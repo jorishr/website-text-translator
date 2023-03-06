a5f6f0f390ae065fbf14faad0ba463bc30654466
jorishr <joris@liondigits.com>
2023-03-06 21:37:50 +0100

    Bug fix: forEach changed to for loop Refactor: clearer naming of data objects to avoid confusion

--- src/translate/index.js
+++ src/translate/index.js
@@ -13,6 +13,7 @@ export default async (
   src,
   dest
 ) => {
+  const prefix = config.fileNames.prefix;
   const [changedValues, newValues] = getValues(keysToTranslate, data);
   log("translateStart", "start2");
   targets.forEach(async (lang) => {
@@ -26,29 +27,28 @@ export default async (
       resNewVals = await getTranslations(newValues, lang, "new");
     }
     //read the existing translation file for the language
-    const prefix = config.fileNames.prefix;
-    let data = getJsonData(src, `${prefix}${lang}.json`) || {};
-    if (Object.keys(data).length !== 0) {
+    let jsonData = getJsonData(src, `${prefix}${lang}.json`) || {};
+    if (Object.keys(jsonData).length !== 0) {
       log("langFileExists", "info", [lang]);
       keysToDelete.forEach((key) => {
-        delete data[key];
+        delete jsonData[key];
       });
       //new values are appended to the end of the file
       for (let i = 0; i < resNewVals.length; i++) {
-        data[offset + i] = resNewVals[i];
+        jsonData[offset + i] = resNewVals[i];
       }
       //changed values are updated in the file at their respective keys
       for (let i = 0; i < resChangedVals.length; i++) {
-        data[keysToTranslate.changedKeys[i]] = resChangedVals[i];
+        jsonData[keysToTranslate.changedKeys[i]] = resChangedVals[i];
       }
     } else {
       log("langFileNew", "info", [lang]);
       //in this case, there should be no changed keys, only new keys to append to the end of the file
       for (let i = 0; i < resNewVals.length; i++) {
-        data[offset + i] = resNewVals[i];
+        jsonData[offset + i] = resNewVals[i];
       }
     }
-    writeFile(dest, data, `txt_data_${lang}.json`, "json");
+    writeFile(dest, jsonData, `${prefix}${lang}.json`, "json");
   });
 };
 
