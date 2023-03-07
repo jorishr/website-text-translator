15d175314f6ac67b0184c3817f14438d7cb0899f
jorishr <joris@liondigits.com>
2023-03-07 21:08:19 +0100

    Added: basic setup for Commander.js CLI argument processing.

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,25 @@
 #!/usr/bin/env node
 
 import main from "../src/index.js";
+import { Command } from "commander";
+const program = new Command();
 
-main();
+program
+  .name("\nHTML-Text-Translator")
+  .description(
+    "Automatically translate the text in html files to multiple languages, store translations in JSON files and detect changes."
+  )
+  .version("0.1.0");
+
+program
+  .command("strip")
+  .description(
+    "Remove all txt-id's from the HTML file(s). No JSON files will be touched but the link with exisiting JSON translations will broken. Proceed with caution."
+  )
+  .action(() => {
+    console.log("Strip HTML files");
+  });
+
+program.parse();
+
+//main();
